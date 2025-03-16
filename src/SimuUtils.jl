3e6bdc1dc18ab4d0a3d9b0a28eae9829e340279c
Jose Guzman <pepetavo@pm.me>
2025-03-16 23:00:16 +0000

    Updated UOM definitions

--- src/SimuUtils.jl
+++ src/SimuUtils.jl
@@ -2,10 +2,10 @@ module SimuUtils
 
 # Include all  functions
 include("helpers/numeric.jl");
+include("helpers/uom.jl");
 include("geometry/area.jl");
 
 # export functions
 export round_to, area_circle;
-
 
 end
---
+++ src/helpers/uom.jl
@@ -0,0 +1,139 @@
+# Import necessary functions from Base to extend them
+import Base: +, -, *, /
+
+# Unit of measure definitions
+abstract type Unit end
+
+@enum VolumeUnit begin
+    BBL  # Barrels
+    M3   # Cubic meters
+    FT3  # Cubic feet
+    GAL  # Gallons
+end
+
+@enum LengthUnit begin
+    M    # Meters
+    FT   # Feet
+    IN   # Inches
+end
+
+@enum RateUnit begin
+    BPM  # Barrels per minute
+    M3MIN  # Cubic meters per minute
+    LPM  # Liters per minute
+end
+
+@enum VelocityUnit begin
+    FT_S  # Feet per second
+    FT_MIN  # Feet per minute
+    M_S  # Meters per second
+    M_MIN  # Meters per minute
+end
+
+@enum MassUnit begin
+    KG  # Kilograms
+    LB  # Pounds
+end
+
+
+# UOM definition
+#
+# The UOM struct is used to define the unit of measure for a given quantity.
+# It is a parametric type that takes two type parameters: T and U. T is the
+struct Uom{T, U<:Unit}
+    value::T
+    unit::U
+end
+
+# Conversion factors
+"""
+	UOM_CONVERSIONS
+
+A dictionary of conversion factors between different units of measure.
+"""
+const UOM_CONVERSIONS = Dict(
+	# volume
+    (BBL, M3) => 0.159,
+    (BBL, FT3) => 0.159 / 0.3048^3,
+    (BBL, GAL) => 42.0,
+    (M3, BBL) => 1 / 0.159,
+    (M3, FT3) => 1 / 0.3048^3,
+    (M3, GAL) => 1_000 / 3.785,
+    (FT3, BBL) => 0.3048^3 / 0.159,
+    (FT3, M3) => 0.3048^3,
+    (FT3, GAL) =>  0.3048^3 * 1_000 / 3.785,
+    (GAL, BBL) => 1 / 42.0,
+    (GAL, M3) => 3.785 / 1_000,
+    (GAL, FT3) => 3.785 / (1_000 * 0.3048^3),
+    # length
+    (M, FT) => 1 / 0.3048,
+    (M, IN) => 1 / 0.0254,
+    (FT, M) => 0.3048,
+    (FT, IN) => 12.0,
+    (IN, M) => 0.0254,
+    (IN, FT) => 1 / 12.0,
+    # rate
+    (BPM, M3MIN) => 0.159,
+    (M3MIN, BPM) => 1 / 0.159,
+    (BPM, LPM) => 42.0 / 3.785,
+    (LPM, BPM) => 3.785 / 42.0,
+    # mass
+    (KG, LB) => 1 / 0.454,
+    (LB, KG) => 0.454,
+    # velocity
+    (FT_S, FT_MIN) => 60.0,
+    (FT_S, M_S) => 0.3048,
+    (FT_S, M_MIN) => 0.3048 * 60.0,
+    (FT_MIN, FT_S) => 1 / 60.0,
+)
+
+# Conversion functions
+"""
+	convert(value::T, from_unit::U, to_unit::U) where {T<:Number, U<:Unit}
+
+Convert a value from one unit of measure to another.
+
+# Arguments
+- `value::T`: The value to convert.
+- `from_unit::U`: The unit of measure to convert from.
+- `to_unit::U`: The unit of measure to convert to.
+
+# Returns
+- `T`: The converted value.
+"""
+function convert(value::T, from_unit::U, to_unit::U) where {T<:Number, U<:Unit}
+    if from_unit == to_unit
+        return value
+    end
+    factor = get(UOM_CONVERSIONS, (from_unit, to_unit), nothing)
+    if factor === nothing
+        throw(ArgumentError("""
+        	Conversion from $from_unit to $to_unit is not defined or
+	        units are not compatible.
+		"""))
+    end
+    return value * factor
+end
+
+function convert(uom::Uom{T, U}, to_unit::U) where {T, U<:Unit}
+	new_value = convert(uom.value, uom.unit, to_unit)
+	return Uom{T, U}(new_value, to_unit)
+end
+
+function +(uom1::Uom{T, U}, uom2::Uom{T, U}) where {T, U<:Unit}
+	uom2_converted = convert(uom2, uom1.unit)
+	return Uom{T, U}(uom1.value + uom2_converted.value, uom1.unit)
+end
+
+function -(uom1::Uom{T, U}, uom2::Uom{T, U}) where {T, U<:Unit}
+	uom2_converted = convert(uom2, uom1.unit)
+	return Uom{T, U}(uom1.value - uom2_converted.value, uom1.unit)
+end
+
+function *(uom::Uom{T, U}, scalar::T) where {T, U<:Unit}
+	return Uom{T, U}(uom.value * scalar, uom.unit)
+end
+
+function /(uom::Uom{T, U}, scalar::T) where {T, U<:Unit}
+	return Uom{T, U}(uom.value / scalar, uom.unit)
+end
